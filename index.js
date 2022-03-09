8bf8419ba697326c993d9c8744ad03df9994b4a3
canicemichael <canicemichael@gmail.com>
2022-03-09 12:05:34 +0100

    Update wrote a post route sample for adding campgrounds in index.js

--- index.js
+++ index.js
@@ -9,38 +9,35 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 
-main().catch(err => console.log(err));
+mongoose.connect("mongodb+srv://canice:canice@cluster0.anmxw.mongodb.net/yelp-camp?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(()=> console.log('DB connection successful'))
 
-async function main() {
-    await mongoose.connect('mongodb+srv://canice:<password>@cluster0.anmxw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
-}
+const campgroundSchema = new mongoose.Schema({
+    name: String,
+    image: String
+});
 
-let campgrounds = [
-    {name: "Salmon Creek", image: "https://media.istockphoto.com/photos/motor-home-and-sunset-picture-id1321202626?b=1&k=20&m=1321202626&s=170667a&w=0&h=-Xggj1lySaS1HQvEXBTy2Ba4_rPUEyR3nt40les3ues="},
-    {name: "Catalynk West", image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FtcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"},
-    {name: "Ebube Nnenna", image: "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2FtcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"},
-    {name: "Salmon Creek", image: "https://media.istockphoto.com/photos/motor-home-and-sunset-picture-id1321202626?b=1&k=20&m=1321202626&s=170667a&w=0&h=-Xggj1lySaS1HQvEXBTy2Ba4_rPUEyR3nt40les3ues="},
-    {name: "Catalynk West", image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FtcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"},
-    {name: "Ebube Nnenna", image: "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2FtcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"},
-    {name: "Salmon Creek", image: "https://media.istockphoto.com/photos/motor-home-and-sunset-picture-id1321202626?b=1&k=20&m=1321202626&s=170667a&w=0&h=-Xggj1lySaS1HQvEXBTy2Ba4_rPUEyR3nt40les3ues="},
-    {name: "Catalynk West", image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8Y2FtcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"},
-    {name: "Ebube Nnenna", image: "https://images.unsplash.com/photo-1492648272180-61e45a8d98a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8Y2FtcHxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60"}
-];
+const Campground = mongoose.model('Campground', campgroundSchema);
+
 
 app.get('/', (req, res) => {
     res.render("landing");
 });
 
 app.get("/campgrounds", (req, res) => {
-    res.render("campgrounds", {campgrounds: campgrounds});
+    Campground.find({}, (err, camps) => {
+        if(err){
+            console.log('error: ' + err);
+        }else {
+            res.render("campgrounds", {campgrounds: camps});
+            console.log(camps);
+        }
+    });
 });
 
-app.post("/campgrounds", (req, res) => {
-    let name = req.body.name;
-    let image = req.body.image;
-    let newCamp = { name: name, image: image };
-    campgrounds.push(newCamp);
-
+app.post("/campgrounds", async (req, res) => {
+    const camps = new Campground(req.body);
+    await camps.save();
     res.redirect('/campgrounds');
 });
 
