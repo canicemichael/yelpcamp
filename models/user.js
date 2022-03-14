b0b490b548caeafca270fe4026b322cf03c2086d
canicemichael <canicemichael@gmail.com>
2022-03-14 21:33:16 +0100

    Add wrote the user model

---
+++ models/user.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    isAdmin: {
+        type: Boolean,
+        default: false
+    }
+}, { timestamps: true });
+
+const User = mongoose.model('User', UserSchema);
+
+const validateUser = (data) => {
+    const schema = Joi.object({
+        username: Joi.string().required().unique(),
+        email: Joi.string().required().unique(),
+        password: Joi.string().required(),
+        isAdmin: Joi.boolean().default(false)
+    })
+    return schema.validate(data);
+}
+
+exports.User = User;
+exports.validateUser = validateUser;
